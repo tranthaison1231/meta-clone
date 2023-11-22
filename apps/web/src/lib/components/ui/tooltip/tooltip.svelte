69a50fdd26fcb4cdb5cb6f8c97eb7153e8d3565f
son.tran <son.tran@team.enouvo.com>
2023-11-22 09:40:22 +0700

    feat(master): update communities

---
+++ apps/web/src/lib/components/ui/tooltip/tooltip.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import { Tooltip as TooltipPrimitive } from 'bits-ui';
+	import { cn } from '$lib/utils/cn';
+	import { flyAndScale } from '$lib/utils/flyAndScale';
+
+	export let content = '';
+	export let className: string | undefined = undefined;
+</script>
+
+<TooltipPrimitive.Root>
+	<TooltipPrimitive.Trigger>
+		<slot />
+	</TooltipPrimitive.Trigger>
+	<TooltipPrimitive.Content
+		transition={flyAndScale}
+		transitionConfig={{
+			y: 8,
+			duration: 150
+		}}
+		sideOffset={4}
+		class={cn(
+			'bg-popover text-popover-foreground z-50 overflow-hidden rounded-md border px-3 py-1.5 text-sm shadow-md',
+			className
+		)}
+		{...$$restProps}
+	>
+		{content}
+	</TooltipPrimitive.Content>
+</TooltipPrimitive.Root>
---
+++ apps/web/src/routes/(auth)/Communities.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { communitiesApi } from '$lib/apis/communities';
+	import Tooltip from '$lib/components/ui/tooltip/tooltip.svelte';
+	import { useQuery } from '@sveltestack/svelte-query';
+
+	const result = useQuery(['communities'], () => communitiesApi.getAll());
+</script>
+
+<div class="space-y-4">
+	{#each $result.data?.data?.communities ?? [] as { name, logo }}
+		<Tooltip content={name}>
+			<button class="rounded-xl p-1 hover:bg-gray-100">
+				<img src={logo} alt={name} class="rounded-xl" />
+			</button>
+		</Tooltip>
+	{/each}
+</div>
