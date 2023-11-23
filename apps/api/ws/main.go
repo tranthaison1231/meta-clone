804b1a7e0e345c1420dfe30638df4df07907d006
son.tran <son.tran@team.enouvo.com>
2023-11-23 16:21:47 +0700

    feat(master): add socket

--- apps/api/ws/main.go
+++ apps/api/ws/main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/tranthaison1231/meta-clone/api/handlers"
 )
+
+type Body struct {
+	action string
+}
 
 func Handler(ctx context.Context, event events.APIGatewayWebsocketProxyRequest) (events.APIGatewayProxyResponse, error) {
 	_, err := json.Marshal(event)
@@ -16,15 +19,21 @@ func Handler(ctx context.Context, event events.APIGatewayWebsocketProxyRequest) 
 		return events.APIGatewayProxyResponse{}, err
 	}
 
-	fmt.Print(event.RequestContext.RouteKey)
+	body := Body{}
+	json.Unmarshal([]byte(event.Body), &body)
 
 	switch event.RequestContext.RouteKey {
 	case "$connect":
 		return handlers.Connect(ctx, event)
 	case "$disconnect":
 		return handlers.Disconnect(ctx, event)
-	case "SEND_MESSAGE":
-		return handlers.SendMessageSocket(ctx, event)
+	case "$default":
+		switch body.action {
+		case "SEND_MESSAGE":
+			return handlers.SendMessageSocket(ctx, event)
+		default:
+			return events.APIGatewayProxyResponse{Body: "no handler", StatusCode: 200}, nil
+		}
 	default:
 		return events.APIGatewayProxyResponse{Body: "no handler", StatusCode: 200}, nil
 	}
