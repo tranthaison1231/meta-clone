d929a4d0916cd8de6c429d0cf1f32a49e3c39de6
son.tran <son.tran@team.enouvo.com>
2023-11-26 02:16:32 +0700

    feat(master): refactor response.go

--- apps/api/helpers/response.go
+++ apps/api/helpers/response.go
@@ -7,29 +7,29 @@ import (
 )
 
 func Success(c *gin.Context, data interface{}) {
-	c.JSON(http.StatusOK, gin.H{"status": "success", "data": data, "code": 200})
+	c.JSON(http.StatusOK, gin.H{"status": "success", "data": data, "code": http.StatusOK})
 }
 
 func Fail400(c *gin.Context, msg string) {
-	c.JSON(http.StatusBadRequest, gin.H{"status": "fail", "message": msg, "code": 400})
+	c.JSON(http.StatusBadRequest, gin.H{"status": "fail", "message": msg, "code": http.StatusBadRequest})
 }
 
 func Fail401(c *gin.Context, msg string) {
-	c.JSON(http.StatusUnauthorized, gin.H{"status": "fail", "message": msg, "code": 401})
+	c.JSON(http.StatusUnauthorized, gin.H{"status": "fail", "message": msg, "code": http.StatusUnauthorized})
 }
 
 func Fail403(c *gin.Context, msg string) {
-	c.JSON(http.StatusForbidden, gin.H{"status": "fail", "message": msg, "code": 403})
+	c.JSON(http.StatusForbidden, gin.H{"status": "fail", "message": msg, "code": http.StatusForbidden})
 }
 
 func Fail404(c *gin.Context, msg string) {
-	c.JSON(http.StatusNotFound, gin.H{"status": "fail", "message": msg, "code": 404})
+	c.JSON(http.StatusNotFound, gin.H{"status": "fail", "message": msg, "code": http.StatusNotFound})
 }
 
 func Fail409(c *gin.Context, msg string) {
-	c.JSON(http.StatusConflict, gin.H{"status": "fail", "message": msg, "code": 409})
+	c.JSON(http.StatusConflict, gin.H{"status": "fail", "message": msg, "code": http.StatusConflict})
 }
 
 func Fail502(c *gin.Context, msg string) {
-	c.JSON(http.StatusBadGateway, gin.H{"status": "fail", "message": msg, "code": 502})
+	c.JSON(http.StatusBadGateway, gin.H{"status": "fail", "message": msg, "code": http.StatusBadGateway})
 }
